1ca9e04cb78825ecc0d450770d438a277f3127d8
Vamsi kaki <vamsi@myaibo.in>
2025-08-20 13:06:08 +0530

    build fix

--- src/pages/api/unsubscribe.ts
+++ src/pages/api/unsubscribe.ts
@@ -32,6 +32,7 @@ export default async function handler(req: NextApiRequest, res: NextApiResponse)
 
     res.status(200).json({ success: true });
   } catch (err) {
+    console.error(err)
     res.status(500).json({ error: "Failed to send email" });
   }
 }
